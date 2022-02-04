d15209a4fd3c33abb6372283e3b012b60cb82f3e
Paolo <paoloagva@gmail.com>
2022-02-04 13:55:20 +0100

    Formatting changes

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,34 +1,28 @@
-import React, { Component } from "react";
-import QuizQuestionButton from "./QuizQuestionButton";
+import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
-    render() {
-        const handleClick = (buttonText) => {};
-        return (
-            <main>
-                <section>
-                    <p>{this.props.quiz_question.instruction_text}</p>
-                </section>
-                <section className="buttons">
-                    <ul>
-                        {this.props.quiz_question.answer_options.map(
-                            (element, index) => {
-                                return (
-                                    <QuizQuestionButton
-                                        key={index}
-                                        clickHandler={this.handleClick.bind(
-                                            this
-                                        )}
-                                        button_text={element}
-                                    ></QuizQuestionButton>
-                                );
-                            }
-                        )}
-                    </ul>
-                </section>
-            </main>
-        );
+  handleClick(buttonText) {
+    if(buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler()
     }
+  }
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
+            })}
+          </ul>
+        </section>
+      </main>      
+    )
+  }
 }
 
-export default QuizQuestion;
+export default QuizQuestion
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
-    render() {
-        const handleClick = () => {
-            this.props.clickHandler(this.props.button_text);
-        };
-        return (
-            <li>
-                <button onClick={this.handleClick.bind(this)}>
-                    {this.props.button_text}
-                </button>
-            </li>
-        );
-    }
+  handleClick() {
+    this.props.clickHandler(this.props.button_text)
+  }
+  render() {
+    return (
+      <li>
+        <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
+      </li>
+    )
+  }
 }
 
-export default QuizQuestionButton;
+export default QuizQuestionButton
